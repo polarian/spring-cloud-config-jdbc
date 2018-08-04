bd2909dd679d2155ef6686bfbc4b13819e077a4b
polarian <stpolarian@gmail.com>
2018-08-04 20:50:15 +0900

    add settings to encrypt properties (JCE 8 required)

--- server/src/main/resources/data-h2.sql
+++ server/src/main/resources/data-h2.sql
@@ -2,4 +2,4 @@ INSERT INTO PROPERTIES (application, profile, label, key, value) VALUES('ConfigC
 INSERT INTO PROPERTIES (application, profile, label, key, value) VALUES('ConfigClient', 'production', 'latest', 'spring.datasource.url', 'jdbc:mysql://localhost:3306/production?useUnicode=true&characterEncoding=utf8');
 INSERT INTO PROPERTIES (application, profile, label, key, value) VALUES('ConfigClient', 'production', 'latest', 'spring.datasource.driverClass', 'com.mysql.jdbc.Driver');
 INSERT INTO PROPERTIES (application, profile, label, key, value) VALUES('ConfigClient', 'production', 'latest', 'spring.datasource.username', 'production');
-INSERT INTO PROPERTIES (application, profile, label, key, value) VALUES('ConfigClient', 'production', 'latest', 'spring.datasource.password', 'Re@lP@ssword');
+INSERT INTO PROPERTIES (application, profile, label, key, value) VALUES('ConfigClient', 'production', 'latest', 'spring.datasource.password', '{cipher}4560f389df088579e8ed952174d88a77bf0729fbbae118e16c784ac1bc2bb4f0');
